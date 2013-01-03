1402a604ca329e59c77d431f3bb5b8a8b29a2c6b
Lucas Paulger <lucas.paulger@gmail.com>
2013-01-03 15:31:35 -0500

    remove jscover folders

--- example-filesystem.sh
+++
@@ -1,2 +0,0 @@
-#java -jar target/dist/JSCover.jar -fs --branch --no-instrument=test doc/example-qunit/src doc/example-qunit/out
-java -jar target/dist/JSCover-all.jar -fs --branch --no-instrument=jasmine-standalone-1.3.1 assets/themes/LP/js target/report
