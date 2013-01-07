a25977b756fb0bb9e8e5e5dd64a5e340f772078a
Lucas Paulger <lucas.paulger@gmail.com>
2013-01-07 16:28:21 -0500

    file name change

---
+++ target/report/original-src/app/TwitterApi.js
@@ -0,0 +1,34 @@
+(function() {
+  /**
+   * Simple javascript API class that handles requests to twitter API
+   * @param {object} config holds any configurable values used within the api
+   */
+
+  function TwitterApi(config) {
+    var self = this;
+
+    self.url = config.twitter.baseUrl;
+    self.username = config.twitter.username;
+
+    /**
+     * returns most recent user tweets
+     * @return {Tweets}
+     */
+    self.getRecentPosts = function(options) {
+
+      var count = options.count;
+
+      return $.ajax({
+        url: self.url + '/statuses/user_timeline.json?',
+        data: {
+          'screen_name': self.username,
+          'count': count
+        },
+        dataType: "jsonp",
+        timeout: 15000
+      });
+    };
+  }
+
+  window.TwitterApi = TwitterApi;
+}());
